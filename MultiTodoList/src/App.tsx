7059a97f7c539e608a8865ec3d12c64936690bfe
Hassan Hashmi <hhashmi@janacorporation.com>
2023-05-18 14:00:55 -0400

    added basic elements, working on styling first page before starting to add logic and functions

--- MultiTodoList/src/App.tsx
+++ MultiTodoList/src/App.tsx
@@ -6,8 +6,19 @@ function App() {
 
   return (
     <>
-      <h1 className='font-bold'>Multi to Do list</h1>
-      <div className="card">
+      <div className='flex-row sm:flex-col'>
+        <h1 className='items-start font-sans text-4xl' >Multi ToDo</h1>
+        <div className='space-x-4 text-right items-end'>
+          <button className='bg-green-300 p-1'>
+            Add New Lists
+          </button>
+          <button className='bg-rose-500 p-1'>
+            Delete All Lists
+          </button>
+        </div>
+
+      </div>
+      <div className="">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
