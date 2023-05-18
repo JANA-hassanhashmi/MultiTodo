14ce46e8591dcd8fffbb927c1dd5cb4a6b149d12
Hassan Hashmi <hhashmi@janacorporation.com>
2023-05-18 16:38:56 -0400

    added some more styling to front empty page

--- MultiTodoList/src/App.tsx
+++ MultiTodoList/src/App.tsx
@@ -11,12 +11,12 @@ function App() {
       <div className='flex flex-col md:justify-between md:flex-row'>
         <h1 className='font-sans text-3xl font-semibold text-slate-900' >Multi ToDo</h1>
         <div className='space-x-2 text-white inline-flex items-baseline'>
-          <div className='bg-green-500 rounded py-1 px-3 inline-flex items-baseline space-x-1.5'>
+          <div className='bg-green-500 rounded py-1 px-3 inline-flex items-baseline space-x-1.5 cursor-pointer hover:bg-green-600'>
           <FaPlus className='scale-110'/>
           <span className=''>Add New Lists</span>
           </div>
 
-          <div className='bg-rose-500 rounded py-1 px-3 inline-flex items-baseline space-x-1.5'>
+          <div className='bg-rose-300 rounded py-1 px-3 inline-flex items-baseline space-x-1.5 cursor-pointer hover:bg-rose-500'>
           <MdDelete className='scale-125'/>
           <span className=''>Delete All Lists</span>
           </div>
